5c19b081f49f6117f88753b4d421433917f3be74
Braytner Bruno <49925138+braytnerb@users.noreply.github.com>
2025-02-13 15:30:28 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # clone-tabnews#Clone do https://.www.TabNews.com.br com a finalidade de exercitar as aulas do curso https://curso.dev do Filipe Deschamps.
-
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-  return <h1> ADILA MEU AMOR E ANALU TB E A KAINHA</h1>
+  return <h1> ADILA MEU AMOR E ANALU TB E A KAINHA</h1>;
 }
 
+function Home2() {
+  console.log("teste 2");
+}
 export default Home;
