00b008979dd116e8ec94e3875538477ece75ce26
Braytner Bruno <49925138+braytnerb@users.noreply.github.com>
2025-02-14 00:45:30 +0000

    nome luiz

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <h1>
       {" "}
       ADILA MEU AMOR E ANALU TB E A KAINHA <br></br>aqui voce encontra o que
-      tando espera: QWRpbGEgcXVlciBjYXNhciBjb21pZ28/Cg=={" "}
+      tanto espera: QWRpbGEgcXVlciBjYXNhciBjb21pZ28/Cg== luiz fernando!
     </h1>
   );
 }
