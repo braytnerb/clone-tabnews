b6bf80640f1508325128285657657a8ac00e9ec6
Braytner Bruno <49925138+braytnerb@users.noreply.github.com>
2025-02-14 00:54:27 +0000

    melhorando o pagina

--- pages/Home.js
+++ pages/Home.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import "./styles.css";
+import React, { useState } from "react";
 
 function Home() {
   const [showMessage, setShowMessage] = useState(false);
 
   return (
     <div className="container">
-      <h1 className="title">Adila, Analu e Aika, vocês são meus amores</h1>
+      <h1 className="title">ADILA MEU AMOR E ANALU TB E A KAINHA</h1>
+      <p className="subtitle">Aqui você encontra o que tanto espera:</p>
       <button className="btn" onClick={() => setShowMessage(true)}>
         Mostrar Mensagem
       </button>
---
+++ pages/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./Home";
+import "./styles.css"; // Importando os estilos
+
+function App() {
+  return <Home />;
+}
+
+export default App;
