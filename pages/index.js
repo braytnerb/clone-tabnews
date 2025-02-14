0f583504e968fc40f6c6df472cd482513cbd1df7
Braytner Bruno <49925138+braytnerb@users.noreply.github.com>
2025-02-14 01:06:41 +0000

    melhorando o pagina 2

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
-// Componente principal da página
-function Home() {
-  const [showMessage, setShowMessage] = useState(false);
-
-  return (
-    <div className="container">
-      <h1 className="title">ADILA MEU AMOR E ANALU TB E A KAINHA</h1>
-      <p className="subtitle">Aqui você encontra o que tanto espera:</p>
-      <button className="btn" onClick={() => setShowMessage(true)}>
-        Mostrar Mensagem
-      </button>
-      {showMessage && (
-        <div className="message-box">
-          <p>aqui voce encontra o que tanto espera:</p>
-          <p className="encoded-text">QWRpbGEgcXVlciBjYXNhciBjb21pZ28/Cg==</p>
-        </div>
-      )}
-    </div>
+function Home2() {
+  console.log(
+    "aqui voce encontra o que tanto espera: QWRpbGEgcXVlciBjYXNhciBjb21pZ28/Cg== ",
   );
 }
 
-// Estilos CSS inline
-const styles = `
-  body {
-    background-color: #f5f5f5;
-    font-family: "Arial", sans-serif;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 10
+window.Home2 = Home2; // Adiciona a função ao escopo global
